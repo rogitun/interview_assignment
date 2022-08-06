8931d3d2e57adacef190151872523463e930f723
rogitun <rogitun@gmail.com>
2022-08-07 00:15:01 +0900

    댓글 작성

--- src/test/java/nts/assignment/service/PostServiceTest.java
+++ src/test/java/nts/assignment/service/PostServiceTest.java
@@ -1,11 +1,18 @@
 package nts.assignment.service;
 
 import nts.assignment.config.AESCrypto;
+import nts.assignment.domain.Comment;
+import nts.assignment.domain.Post;
+import nts.assignment.repository.comment.CommentRepository;
 import nts.assignment.repository.post.PostRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +25,8 @@ class PostServiceTest {
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
 
+    @Autowired
+    CommentRepository commentRepository;
 
 
     @Test
@@ -28,6 +37,29 @@ class PostServiceTest {
         System.out.println(encrypt);
 
         System.out.println(passwordEncoder.matches("abc1234",encrypt));
+    }
 
+
+    @Test
+    @Transactional
+    public void addComment(){
+        Post post = Post.builder()
+                .title("test")
+                .content("testCont")
+                .password("1234")
+                .writer("wrtier")
+                .build();
+        Post save = postRepository.save(post);
+
+        Comment comment = new Comment("cwriter","cpwd","ccont",post);
+        Comment save1 = commentRepository.save(comment);
+
+        Optional<Post> byId = postRepository.findById(save.getPostId());
+
+        Assertions.assertThat(byId.isPresent()).isTrue();
+        Post post1 = byId.get();
+        Assertions.assertThat(post1.getComments().size()).isEqualTo(1);
+
+        Assertions.assertThat(save1.getPost()).isNotNull();
     }
 }
